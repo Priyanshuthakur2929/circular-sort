a83d1aa77d2a8b65d521ad11fcd5137db998e233
Your Name <your-email@example.com>
2026-05-30 23:38:31 +0530

    Add 1st.js

---
+++ 1st.js
@@ -0,0 +1,44 @@
+/***let a = 20;
+let b = 5;
+
+console.log("sum of a and b is:", a + b);***/
+/***let a = prompt("Enter your name:");
+console.log(typeof a);***/
+
+
+/***let a = 5;
+let b = 6;
+let c = a; // c = 5
+
+a = b + c; // a= 11
+b = a + c;
+console.log(a);
+console.log(b);
+console.log(c);***/
+
+/***let a = 456;
+let b = 78;
+console.log(Math.floor(a/b));***/
+
+
+//console.log(8=9);
+
+/***let a = 5; 
+let b = ++a; // fist i crement then assign;
+console.log(a, b);***/
+
+/***let a = 11;
+a = a++ + ++a;
+console.log(a); ***/
+
+/***let a = 11 , b = 22;
+c = a + b + a++ + b++ + ++a + ++b;
+console.log("a=" + a);
+console.log("b=" + b);
+console.log("c=" + c); ***///
+
+console.log(Math.sqrt(16));
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10) + 1);
+console.log(Math.ceil(4.2));
+console.log(Math.round(4.5));
