0d6ff991756ded00c2d45138dc2c52edee2b1666
Your Name <your-email@example.com>
2026-06-06 11:58:49 +0530

    Add Guessing no.js

---
+++ Guessing no.js
@@ -0,0 +1,22 @@
+/*let random = Math.floor(Math.random()*100)+1;
+
+let guess = -1;
+
+while(guess!==random){
+    guess = Number(prompt("Guess a number between 1 to 100:"));
+
+if(isNaN(guess) || guess<1 || guess>100){
+    console.log("Invalid input!Please try again.");
+    continue
+
+}
+if(guess>random){
+    console.log("Too high!Try Again");
+
+} else if(guess<random) {
+    console.log("Too low!Try again");
+} else{
+    console.log("Congratulations!You guessed the number"+random);
+}
+}*/
+
